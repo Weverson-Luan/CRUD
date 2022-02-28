ade49180d36b74cb55d2e66f6dd7e7a675e3b224
Weverson-Luan <weverson.luan@smartioninnovation.com.br>
2022-02-27 23:16:44 -0300

    create/entities/domain: address entity has been created.

--- src/useCases/address/address.service.ts
+++ src/useCases/address/address.service.ts
@@ -1,24 +1,30 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { CreateAddressDto } from '../../core/dtos/create-address.dto';
 
-import { User, UserDocument } from './entities/user.entity';
+import {
+  Address,
+  AddressDocument,
+} from '../../users/entities/domain/address/address.entity';
 
 @Injectable()
-export class UsersService {
-  constructor(@InjectModel(User.name) private UserModel: Model<UserDocument>) {}
+export class AddresssService {
+  constructor(
+    @InjectModel(Address.name) private AddressModel: Model<AddressDocument>,
+  ) {}
   async create(createAddressDto: CreateAddressDto) {
-    const userModel = new this.UserModel(createAddressDto);
-    return userModel.save();
+    const AddressModel = new this.AddressModel(createAddressDto);
+    return AddressModel.save();
   }
 
   findAll() {
-    return this.UserModel.find(); //retorna todos do banco
+    return this.AddressModel.find(); //retorna todos do banco
   }
 
   remove(id: string) {
-    return this.UserModel.deleteOne({
+    return this.AddressModel.deleteOne({
       _id: id, //id que eu desejo exluir
     }).exec(); //para esse metodo ser executado com exito
   }
