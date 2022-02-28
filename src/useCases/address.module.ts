ac729c0678af90b8f0540f8f1c5db9c8808b89ca
Weverson-Luan <weverson.luan@smartioninnovation.com.br>
2022-02-27 23:39:39 -0300

    create/module: the module was created to be able to create a new address.

---
+++ src/useCases/address.module.ts
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AddressService } from '../useCases/address/address.service';
+import { addressController } from '../controllers/addres.controllers';
+import {
+  Address,
+  AddressSchema,
+} from '../users/entities/domain/address/address.entity';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Address.name, schema: AddressSchema }]),
+  ],
+  controllers: [addressController],
+  providers: [AddressService],
+})
+export class UsersModule {}
