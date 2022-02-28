f5ef2e6d02c51c7cfeadc83d15a51694b37d688d
Weverson-Luan <weverson.luan@smartioninnovation.com.br>
2022-02-27 23:05:36 -0300

    create/core/dtos: created the dto for creating a new address.

---
+++ src/core/dtos/create-address.dto.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+export class CreateUserDto {
+  uf: string;
+  city: string;
+  zip_code: string;
+  district: string;
+  street: string;
+  number: number;
+  complement: string;
+}
